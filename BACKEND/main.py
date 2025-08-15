66d36e00735c3aa364c5f819035232d84c604998
Keyvano <keyvanoandriansyah.com>
2025-08-15 16:24:27 +0700

    fix deploy vercel 2

---
+++ BACKEND/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+@app.get("/hello")
+async def hello():
+    return {"message": "Hello from FastAPI!"} 
